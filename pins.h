41da58d90d06c8951494561d4c941df8f9bcc302
jimmy <livingoscilloscope@gmail.com>
2019-02-28 16:53:06 -0500

    updated pins.h to match updated schematic

--- pins.h
+++ pins.h
@@ -5,14 +5,23 @@
 #define FLEX_THUMB_PIN A1
 #define FLEX_INDEX_PIN A2
 #define FLEX_MIDDLE_PIN A3
-#define FLEX_RING_PIN A4
-#define FLEX_PINKY_PIN A5
+#define FLEX_PINKY_PIN A4
 
 // pin layout for force sensors
-#define FORCE_THUMB_PIN A6
-#define FORCE_INDEX_PIN A7
+#define FORCE_THUMB_PIN A5
+#define FORCE_INDEX_PIN A6
 
 // pin layout for grip strength selection
 #define FORCE_SELECTOR A0
+
+// pin layout for servo motor
+#define SERVO_PIN 9
+
+// motor power cutoff pin
+#define MOTOR_POWER_CUTOFF 10
+
+// encoder inputs
+#define ENCODER_A 2
+#define ENCODER_B 3
 
 #endif // TENACI_PINS_H
