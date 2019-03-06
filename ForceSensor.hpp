0ece51fc947fb059fbd5e7344e5b4537a8d45006
jimmy <livingoscilloscope@gmail.com>
2019-03-05 22:36:01 -0500

    restructured to not use FreeRTOS

--- ForceSensor.hpp
+++ ForceSensor.hpp
@@ -1,7 +1,16 @@
 #ifndef TENACI_FORCE_SENSOR_H
 #define TENACI_FORCE_SENSOR_H
 
+#include "Sensor.hpp"
 
+class ForceSensor : public Sensor
+{
+public:
+    ForceSensor(struct sensor_data_t *sensor_info);
+    void read();
+protected:
+    double resistor_resistance;
+};
 
 
 #endif // TENACI_FORCE_SENSOR_H
